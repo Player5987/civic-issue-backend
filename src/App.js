9620e3f287f4a6b3e2eccff7ed521f3098a7eb7a
khushbhu-git <khushbhuofficial@gmail.com>
2025-09-18 19:08:23 +0530

    report page is added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import React from "react";
-<<<<<<< HEAD
+import Auth from "./components/login";
+import Home from "./components/home";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Auth />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
+  )}
 import Auth from "./components/login"; // login/register page
 import Home from "./components/home"; // static home page
 import Report from "./components/Report";//report page
@@ -13,18 +23,6 @@ function App() {
     <div className="App">
       {showHome ? <Home /> : <Report />}
     </div>
-=======
-import Auth from "./components/login";
-import Home from "./components/home";
-import { Routes, Route } from "react-router-dom";
-
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Auth />} />
-      <Route path="/home" element={<Home />} />
-    </Routes>
->>>>>>> 0f596ba2188e171bad4e936cbf509d6ea72b6fce
   );
 }
 
